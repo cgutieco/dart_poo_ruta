ab906fc53f7b8c1c6bd4d6b2b64af2fa1acd4315
cesar_gutierrez <cgutieco@outlook.com>
2025-08-20 22:17:56 -0500

    update LibroPrestado class to allow nullable nombreUsuario attribute

--- nivel_03_constructores/ejercicios/biblioteca_de_libros/entities/libro_prestado.dart
+++ nivel_03_constructores/ejercicios/biblioteca_de_libros/entities/libro_prestado.dart
@@ -29,7 +29,7 @@ class LibroPrestado extends Libro {
 
   String? get nombreUsuario => _nombreUsuario;
 
-  set nombreUsuario(String value) {
+  set nombreUsuario(String? value) {
     _nombreUsuario = value;
   }
 
